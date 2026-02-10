315c9bbedcd86affbb96b3bcd94c0e2194ff7e78
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-02-10 14:08:17 +0700

    Pertemuan 1 - langkah 1

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 244107020139<br>Nama: Mokhammad Ilham Putra Wijaya';
 });
