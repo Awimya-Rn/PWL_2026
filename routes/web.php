e8c492948f176978f1e1300cbeebe2e008dae0bc
Mokhammad Ilham Putra W <ilhampolpro45@gmail.com>
2026-02-12 18:36:48 +0700

    jobsheet2-langkah5

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return 'Selamat Datang';
@@ -45,3 +45,12 @@ Route::get('/hello',[WelcomeController::class,"hello"]);
 Route::get('/',[HomeController::class,"home"]);
 Route::get('/about',[AboutController::class,"about"]);
 Route::get('/articles/{id}',[ArticleController::class,"articles"]);
+
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index','show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create','store','update','destroy'
+]);
